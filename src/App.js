9add014c33b13712e76d268ae700dd4d965ec7e5
Christian <christianyip92@gmail.com>
2022-10-13 10:20:30 -0700

    Add intro page elements and styling.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
 
 function App() {
+  const [quizStart, setQuizStart] = false;
+  
   return (
-    <p>test</p>
+    // introPage component
+    <div className='intro-page'>
+      <div className='intro-text'>
+        <h1 className='title'>TV TRIVIA</h1>
+        <p className='message'>Put your totally useful TV knowledge to the test!</p>
+      </div>
+      <button className='start-btn'>Start quiz</button>
+    </div>    
+
+    // questions page
+      // individual question components
   );
 }
 
